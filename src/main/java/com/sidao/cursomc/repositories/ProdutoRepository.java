6ef158d80093d755ff0b542e1d8252b4bd178bdb
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-20 17:56:03 -0300

    produto e associação muitos para muitos

---
+++ src/main/java/com/sidao/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.sidao.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sidao.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+	
+}
+
