1360f4196601fb0dea5423639a046b90136fa1ba
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-21 00:05:05 -0300

    endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/sidao/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.sidao.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sidao.cursomc.domain.Cliente;
+import com.sidao.cursomc.repositories.ClienteRepository;
+import com.sidao.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
