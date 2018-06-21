258717e9bd0b43535ad120d0d837b79ca154d5ca
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-21 02:41:15 -0300

    Pedido, EstadoPagamento e pagamento

--- src/main/java/com/sidao/cursomc/CursomcApplication.java
+++ src/main/java/com/sidao/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.sidao.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.sidao.cursomc.domain.Cidade;
 import com.sidao.cursomc.domain.Cliente;
 import com.sidao.cursomc.domain.Endereco;
 import com.sidao.cursomc.domain.Estado;
+import com.sidao.cursomc.domain.Pagamento;
+import com.sidao.cursomc.domain.PagamentoComBoleto;
+import com.sidao.cursomc.domain.PagamentoComCartao;
+import com.sidao.cursomc.domain.Pedido;
 import com.sidao.cursomc.domain.Produto;
+import com.sidao.cursomc.domain.enums.EstadoPagamento;
 import com.sidao.cursomc.domain.enums.TipoCliente;
 import com.sidao.cursomc.repositories.CategoriaRepository;
 import com.sidao.cursomc.repositories.CidadeRepository;
 import com.sidao.cursomc.repositories.ClienteRepository;
 import com.sidao.cursomc.repositories.EnderecoRepository;
 import com.sidao.cursomc.repositories.EstadoRepository;
+import com.sidao.cursomc.repositories.PagamentoRepository;
+import com.sidao.cursomc.repositories.PedidoRepository;
 import com.sidao.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -91,5 +105,22 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
 	}
 }
--- src/main/java/com/sidao/cursomc/domain/Cliente.java
+++ src/main/java/com/sidao/cursomc/domain/Cliente.java
@@ -34,8 +34,11 @@ public class Cliente implements Serializable{
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
-	@CollectionTable(name="telefone")
+	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente (){
 	}
@@ -105,6 +108,14 @@ public class Cliente implements Serializable{
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,6 +140,6 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
+
 	
 }
