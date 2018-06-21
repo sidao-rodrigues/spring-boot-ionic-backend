5efa90d9bd477f0f7f12d4fbb0e7703dc3e803b8
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-21 16:33:16 -0300

    Atulizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/sidao/cursomc/services/CategoriaService.java
+++ src/main/java/com/sidao/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -28,4 +28,9 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public Categoria update(Categoria obj)
+	{
+		find(obj.getId());
+		return repo.save(obj);
+	}
 }
