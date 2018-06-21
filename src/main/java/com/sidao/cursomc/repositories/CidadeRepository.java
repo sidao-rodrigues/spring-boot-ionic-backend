be0029797ecc03bc5d28d0e1976fa4b5a5be593f
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-20 22:55:08 -0300

    Estado Cidade

---
+++ src/main/java/com/sidao/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,14 @@
+package com.sidao.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sidao.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+}
+
