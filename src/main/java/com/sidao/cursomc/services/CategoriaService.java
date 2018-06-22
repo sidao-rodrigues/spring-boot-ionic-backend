eb7cf60c587e40e995f957aa8db16e6ad8ce00da
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-22 00:52:45 -0300

    Listando todas categorias com DTO

--- src/main/java/com/sidao/cursomc/services/CategoriaService.java
+++ src/main/java/com/sidao/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.sidao.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,5 +48,10 @@ public class CategoriaService {
 		{
 				throw new DataIntegrityException("Não é possível excluir uma categoria que possui Produtos!");
 		}
+	}
+	
+	public List<Categoria> findAll()
+	{
+		return repo.findAll();
 	}
 }
