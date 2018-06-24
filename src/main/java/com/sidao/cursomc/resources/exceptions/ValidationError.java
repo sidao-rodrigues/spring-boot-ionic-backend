c38f6f4be421859f4f10c90b28888fa191a3b1ca
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-24 18:54:11 -0300

    PUT, DELE e GET para cliente

--- src/main/java/com/sidao/cursomc/resources/exceptions/ValidationError.java
+++ src/main/java/com/sidao/cursomc/resources/exceptions/ValidationError.java
@@ -9,8 +9,7 @@ public class ValidationError extends StandardError {
 	private List<FieldMessage> errors = new ArrayList<>();
 	
 	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-		
+		super(status, msg, timeStamp);	
 	}
 
 	public List<FieldMessage> getErrors() {
