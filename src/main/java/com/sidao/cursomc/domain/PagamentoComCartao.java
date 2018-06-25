dba77769e41fa60c6bd7edcdedd25a8b3b1fedf8
Sidney Rodrigues <sidaoswat@hotmail.com>
2018-06-24 23:55:25 -0300

    Inserindo Pedido

--- src/main/java/com/sidao/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/sidao/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.sidao.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.sidao.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 
