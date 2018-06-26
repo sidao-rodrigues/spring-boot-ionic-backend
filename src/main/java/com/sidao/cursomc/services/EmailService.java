ea063e3dd1a768a6650b2a967fd16c8e29ae8b26
sidaoswat <sidaoswat@hotmail.com>
2018-06-26 03:39:07 -0300

    Email HTML

--- src/main/java/com/sidao/cursomc/services/EmailService.java
+++ src/main/java/com/sidao/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.sidao.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.sidao.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
