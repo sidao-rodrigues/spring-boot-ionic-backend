0e22fc49666333aa802b078247c37fbb8e9d851a
sidaoswat <sidaoswat@hotmail.com>
2018-06-28 06:22:54 -0300

    Esqueci a senha

---
+++ src/main/java/com/sidao/cursomc/services/AuthService.java
@@ -0,0 +1,71 @@
+package com.sidao.cursomc.services;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.sidao.cursomc.domain.Cliente;
+import com.sidao.cursomc.repositories.ClienteRepository;
+import com.sidao.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	private Random rand = new Random();
+	
+	public void sendNewPassword(String email) {
+		
+		Cliente cliente = clienteRepository.findByEmail(email);
+		if(cliente == null)
+		{
+			throw new ObjectNotFoundException("Email não encontrado");	
+		}
+		String newPass = newPassword();
+		cliente.setSenha(pe.encode(newPass));
+		
+		clienteRepository.save(cliente);
+		emailService.sendNewPasswordEmail(cliente,newPass);
+	}
+
+	private String newPassword() 
+	{
+		char [] vet = new char[10];
+	
+		for(int x=0;x<10;x++)
+		{
+			vet[x] = randomChar();
+		}
+		return new String(vet);
+	}
+
+	private char randomChar() 
+	{	
+		int opt = rand.nextInt(3);
+		
+		if(opt == 0) //gera digito
+		{
+			return (char) (rand.nextInt(10) + 48);
+		}
+		else if(opt == 1) //gera letra maiuscula
+		{
+			return (char) (rand.nextInt(26) + 65);
+		}
+		else//gera letra minuscula
+		{
+			return (char) (rand.nextInt(26) + 97);
+		}
+
+	}
+	
+}
