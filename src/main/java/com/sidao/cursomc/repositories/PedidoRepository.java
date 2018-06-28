7fa57e1c43dc3db61061fbea5c79d92ee616f469
sidaoswat <sidaoswat@hotmail.com>
2018-06-28 05:56:54 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/sidao/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/sidao/cursomc/repositories/PedidoRepository.java
@@ -1,14 +1,19 @@
 package com.sidao.cursomc.repositories;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.sidao.cursomc.domain.Cliente;
 import com.sidao.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
-	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
 
--- src/main/java/com/sidao/cursomc/services/PedidoService.java
+++ src/main/java/com/sidao/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sidao.cursomc.domain.Cliente;
 import com.sidao.cursomc.domain.ItemPedido;
 import com.sidao.cursomc.domain.PagamentoComBoleto;
 import com.sidao.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.sidao.cursomc.domain.enums.EstadoPagamento;
 import com.sidao.cursomc.repositories.ItemPedidoRepository;
 import com.sidao.cursomc.repositories.PagamentoRepository;
 import com.sidao.cursomc.repositories.PedidoRepository;
+import com.sidao.cursomc.security.UserSS;
+import com.sidao.cursomc.services.exceptions.AuthorizationException;
 import com.sidao.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -72,5 +78,17 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction)
+	{
+		UserSS user = UserService.authenticated();
+		if(user == null)
+		{
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
